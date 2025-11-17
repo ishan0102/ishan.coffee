697017a4bef0730dfec000d9a58c015f64a210b8
Ishan Shah <ishan0102@utexas.edu>
2025-11-17 16:19:43 -0600

    Fix recent notes on mobile

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ishan's Cafe",
+    pageTitle: "Cafe",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.RecentNotes(),
+    Component.DesktopOnly(Component.RecentNotes()),
     Component.Explorer(),
   ],
   right: [
@@ -64,7 +64,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.RecentNotes(),
+    Component.DesktopOnly(Component.RecentNotes()),
     Component.Explorer(),
   ],
   right: [],
